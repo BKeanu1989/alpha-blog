796142d179dbd363397a5b868f2a27bcd66512f0
Kevin Fechner <bkevinfechner1989@gmail.com>
2015-12-01 14:17:38 +0100

    create edit template, action (edit & update)

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -4,9 +4,15 @@ class ArticlesController < ApplicationController
 		@article = Article.new
 	end
 
+	def edit
+		@article = Article.find(params[:id])
+	end
+
 	def create
 		# for rendering the given params for the article || does work without ".inspect"
 		# render plain: params[:article].inspect
+
+		# method article_params for accessing the given oarams
 		@article = Article.new(article_params)
 
 		if @article.save
@@ -17,6 +23,18 @@ class ArticlesController < ApplicationController
 			render 'new'
 		end
 	end
+
+	def update
+		@article = Article.find(params[:id])
+		# needs to be filled with the values of the form fields -> article_params
+		if @article.update(article_params)
+			flash[:notice] = "Article was successfully updated"
+			redirect_to article_path(article_params)
+		else 
+			render 'edit'
+		end
+	end
+
 
 	def show
 		@article = Article.find(params[:id])
