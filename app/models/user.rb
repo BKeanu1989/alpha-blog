0061db833c1d966ae6b059f2433cac664d2bb777
Kevin Fechner <bkevinfechner1989@gmail.com>
2015-12-07 00:29:09 +0100

    signup route, new sign up form/template

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,6 @@
 class User < ActiveRecord::Base
 	has_many :articles
+	has_secure_password
 	before_save { self.email = email.downcase }
 	validates :username, presence: true, uniqueness: { case_sensitive: false }, 
 	length: {minimum: 3, maximum:25}
@@ -7,5 +8,4 @@ class User < ActiveRecord::Base
 	validates :email, presence: true, uniqueness: { case_sensitive: false},
 	length: { maximum: 105 },
 	format: { with: VALID_EMAIL_REGEX }
-	has_secure_password
 end
--- config/routes.rb
+++ config/routes.rb
@@ -10,8 +10,9 @@ Rails.application.routes.draw do
   root 'pages#home'
   get 'about', to: 'pages#about'
 
-
+  get 'signup', to: 'users#new'
   resources :articles
+  resources :users, except: [:new]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
